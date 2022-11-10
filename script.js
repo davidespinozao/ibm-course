01aef865b1a0c73e79b1ae9e121766150673e836
David Espinoza Olguín <despinozao@investigaciones.cl>
2022-11-10 09:12:50 -0300

    Add flex tutorial

---
+++ script.js
@@ -0,0 +1,27 @@
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+
+    var year = new Date().getFullYear() + parseInt(years);
+    if(principal > 0)
+    {
+        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+        return true;
+    }
+    else
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval + " %";
+}
+        
